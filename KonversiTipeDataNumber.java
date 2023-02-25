8895388ca1de437514ee5a735767d978913e6db6
dyaksaa <diasnour0395@gmail.com>
2023-02-25 12:56:52 +0700

    minor

--- KonversiTipeDataNumber.java
+++ KonversiTipeDataNumber.java
@@ -1,6 +1,15 @@
 public class KonversiTipeDataNumber {
 
     public static void main(String[] args) {
+        byte iniByte = 10;
+        short initShort = 100;
+        int iniInt = 1000;
+        long iniLong = 100000;
 
+        byte iniByte2 = (byte) iniInt;
+        int iniInt2 = (int) iniByte;
+
+        System.out.println(iniByte2);
+        System.out.println(iniInt2);
     }
 }
