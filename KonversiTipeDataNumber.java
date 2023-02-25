b0943d8aadb57e875676244b021f11ff320eda65
dyaksaa <diasnour0395@gmail.com>
2023-02-25 12:44:55 +0700

    konversi tipe data number

---
+++ KonversiTipeDataNumber.java
@@ -0,0 +1,6 @@
+public class KonversiTipeDataNumber {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ TipeDataNumber.java
@@ -0,0 +1,16 @@
+public class TipeDataNumber {
+
+    public static void main(String[] args) {
+        byte iniByte = 100;
+        short iniShort = 1000;
+        int iniInt = 100000;
+        long iniLong  = 1000000;
+        long iniLong2 = 10000000L;
+
+        float iniFloat = 10.10F;
+        double iniDouble = 10.11;
+
+        int hexInt = 0xFFFFF;
+        int binInt = 0b01010101;
+    }
+}
