7293ff7111362df55d67e24268368f7b05122baf
dyaksaa <diasnour0395@gmail.com>
2023-04-12 12:39:07 +0700

    operasi matematika

---
+++ OperasiMatematika.java
@@ -0,0 +1,27 @@
+public class OperasiMatematika {
+
+    public static void main(String[] args) {
+        int a = 100;
+        int b = 10;
+
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+
+        int c = 10;
+
+        System.out.println(c += 10);
+        System.out.println(c -= 10);
+        System.out.println(c *= 10);
+        System.out.println(c /= 10);
+
+        int d = +100;
+        int e = -10;
+
+        System.out.println(d++);
+        System.out.println(e--);
+        System.out.println(!true);
+    }
+}
