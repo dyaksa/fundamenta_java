f7aded708f0d21ff400e14895bf741c1fc7fd43d
dyaksaa <diasnour0395@gmail.com>
2023-05-04 10:18:31 +0700

    method

---
+++ Method.java
@@ -0,0 +1,11 @@
+public class Method {
+    public static void main(String[] args) {
+        sayHelloWorld();
+    }
+
+    static void sayHelloWorld() {
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+    }
+}
