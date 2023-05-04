4709ea6ba8e96f5abe8022232c447bc81fee1c7b
dyaksaa <diasnour0395@gmail.com>
2023-05-04 09:17:06 +0700

    do while loop

---
+++ DoWhileLoop.java
@@ -0,0 +1,10 @@
+public class DoWhileLoop {
+    public static void main(String[] args) {
+        var counter = 100;
+
+        do {
+            System.out.println("Perulangan ke - " + counter);
+            counter++;
+        }while (counter <= 10);
+    }
+}
