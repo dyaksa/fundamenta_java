56b071ba21f3ae9198e63224785d1fd879a4eec7
dyaksaa <diasnour0395@gmail.com>
2023-05-05 08:05:26 +0700

    method parameter

---
+++ MethodParameter.java
@@ -0,0 +1,10 @@
+public class MethodParameter {
+    public static void main(String[] args) {
+        sayHello("Dyaksa", "Jauharuddin");
+        sayHello("Putri", "Oktaviani");
+    }
+
+    static void sayHello(String firstName, String lastName){
+        System.out.println("Hello Selamat Pagi " + firstName + " " + lastName);
+    }
+}
