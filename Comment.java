38a527d0a14b7ad6ca43585cd1d2d45047d021f0
dyaksa <diasnour0395@gmail.com>
2024-03-25 14:02:21 +0700

    comment

---
+++ Comment.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class Comment {
+    public static void main(String[] args) {
+        System.out.println(sum(10,10));
+    }
+
+    /** Method Penjumlahan
+     * @param value1 int
+     * @param value2 int
+     * @return int
+     * */
+    static int sum(int value1, int value2) {
+        return value1 + value2;
+    }
+}
